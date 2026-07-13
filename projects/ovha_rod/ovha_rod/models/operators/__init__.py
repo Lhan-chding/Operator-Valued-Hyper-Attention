10cd0eb2799a71a74097f7edd86996ca1da76ac7
Lhan-chding <1223128329@qq.com>
2026-07-13 21:21:17 +0800

    test: cover Q-SRO lazy exports

--- projects/ovha_rod/ovha_rod/models/operators/__init__.py
+++ projects/ovha_rod/ovha_rod/models/operators/__init__.py
@@ -13,7 +13,7 @@ from .relation_fields import RELATION_TYPES, RelationFieldBank
 from .rceo import RCEO, RCEOResult
 from .rqgo import RQGO, RQGOResult
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover - static imports only.
     from .qsro import QSRO, QuerySpatialRelationOperator
 
 _LAZY_QSRO_EXPORTS = frozenset({"QSRO", "QuerySpatialRelationOperator"})
